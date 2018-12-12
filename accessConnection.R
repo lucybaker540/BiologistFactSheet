f6e0bc30e46fc039adea5524d95fbfe3947a4291
Baker <Lucy.Baker@deq.virginia.gov>
2018-12-12 14:41:04 -0500

    end of Emma Session 1

---
+++ accessConnection.R
@@ -0,0 +1,43 @@
+# R 3.5.1
+
+# This file walks you through connecting MS Access to R
+# MS Access is dumb and needs to be run in 32bit R
+
+# packages
+library(tidyverse)
+library(RODBC)
+
+
+# Make connection with Access
+channel <- odbcConnectAccess2007('data/EDASxp_Family_090117.accdb')
+
+# Pull tables you need
+SCIquery <- sqlQuery(channel, 'SELECT * FROM SCIQuery')
+
+totalHab <- sqlQuery(channel,'SELECT * FROM HabValues')
+
+totHabCon <- sqlQuery(channel,'SELECT * FROM HabSamps')
+# Lucy: pull in Stations table and practice joining to other tables
+
+## Lucy assignment:
+#function to filter by 1 station
+# lucy stretch: filter above by date rance, or year(s)
+
+
+# Total hab mess around
+totalHabitat <- left_join(totalHab,totHabCon, by=c('HabSampID', 'Comments','EnterDate'))
+
+rm(totHabCon); rm(totalHab)
+# test manipulation zone
+
+totHab <- totalHabitat %>%
+  group_by(HabSampID, CollDate, StationID) %>%
+  mutate(`Total Habitat` = sum(HabValue))
+
+# plot seasonal/annual total hab scores
+
+totHabWide <- select(totHab, StationID, CollDate, `Total Habitat`, HabParameter, HabValue) %>%
+  spread(HabParameter,HabValue)
+
+# make table of high/low gradient variables:
+# in BenthicStressorReportTemplate_VSCI.Rmd ctrl+F habData ~line 611
